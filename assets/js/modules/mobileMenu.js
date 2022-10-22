71af80f7a122f401714e0ea3f2cab442aa576a62
Wellington Santos <wellington1998santoss@gmail.com>
2022-10-22 15:18:42 -0300

    refactoring mobile menu

--- assets/js/modules/mobileMenu.js
+++ assets/js/modules/mobileMenu.js
@@ -1,6 +1,6 @@
 export default function mobileMenu() {
   const btnMenu = document.querySelector('button.btn-menu');
-  const menu = document.querySelector('header nav.mobile');
+  const menu = document.querySelector('header nav.menu');
   const menuItems = menu.querySelectorAll('ul li');
 
   menuItems.forEach((item) => {
@@ -8,46 +8,22 @@ export default function mobileMenu() {
       const windowWidth = window.innerWidth;
 
       if (menu.classList.contains('active') && windowWidth <= 768) {
-        animElement(menu, 0, -windowWidth);
         btnMenu.classList.remove('active');
-
-        setTimeout(() => {
-          menu.classList.remove('active');
-        }, 300);
+        document.body.classList.remove('overflow');
+        menu.classList.remove('active');
       }
     });
   });
 
   btnMenu.addEventListener('click', () => {
-    const windowWidth = window.innerWidth;
-
     if (menu.classList.contains('active')) {
-      animElement(menu, 0, -windowWidth);
       btnMenu.classList.remove('active');
       document.body.classList.remove('overflow');
-
-      setTimeout(() => {
-        menu.classList.remove('active');
-      }, 300);
+      menu.classList.remove('active');
     } else {
       document.body.classList.add('overflow');
       menu.classList.add('active');
       btnMenu.classList.add('active');
-      animElement(menu, -windowWidth, 0);
     }
   });
-}
-
-function animElement(element, from, to) {
-  element.animate(
-    [
-      { transform: `translateX(${from}px)` },
-      { transform: `translateX(${to}px)` },
-    ],
-    {
-      duration: 300,
-      fill: 'forwards',
-      easing: 'ease-out',
-    },
-  );
 }
